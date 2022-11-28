b87bf9b64308744ab851a61a2d45d39473ff5fd4
Dylanstiven27 <dilanstivenmuozsernad@gmail.com>
2022-11-28 11:35:52 -0500

    se hizo otro linkeo y cambio de link con cambios de nombre para diferenciar

--- js/preguntas.js
+++ js/preguntas.js
@@ -1,8 +1,3 @@
-let basedepreguntas=[
-    {
-        pregunta:"¿como se llama el actual presidente de colombia",
-        imagen:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Casa-narino-1-fachada.jpg/1200px-Casa-narino-1-fachada.jpg",
-        respuesta:"Gustavo petro",
-        distractores: ["Ivan duque","Donald trumpo","Nicolas maduro","Lopez obrador"],
-    },
-];
+function cargarpregunta(index){
+
+}
